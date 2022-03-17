6c1ecef2248a692689e203be7c58e0738200501b
Valeri <veltimofeeva@gmail.com>
2022-03-17 10:21:41 +0400

    added program and tests

--- programs/gm-solana/src/lib.rs
+++ programs/gm-solana/src/lib.rs
@@ -1,15 +1,60 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FsVne7rYcGPKysLvuTzFz1D9mD8iu7QGxEL8xFFypGU9");
 
 #[program]
 pub mod gm_solana {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.gm_count = 0;
+
+        Ok(())
+    }
+
+    pub fn say_gm(ctx: Context<SayGm>, message: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let message = message.clone();
+        let timestamp = Clock::get().unwrap().unix_timestamp;
+        let user = *ctx.accounts.user.to_account_info().key;
+        let gm = GmMessage {
+            user,
+            message,
+            timestamp,
+        };
+        base_account.gm_list.push(gm);
+        base_account.gm_count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 1024)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub gm_count: u64,
+    pub gm_list: Vec<GmMessage>,
+}
+
+#[derive(Clone, Debug, AnchorSerialize, AnchorDeserialize)]
+pub struct GmMessage {
+    pub user: Pubkey,
+    pub message: String,
+    pub timestamp: i64,
+}
+
+#[derive(Accounts)]
+pub struct SayGm<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    pub user: Signer<'info>,
+}
--- tests/gm-solana.ts
+++ tests/gm-solana.ts
@@ -1,16 +1,63 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { GmSolana } from "../target/types/gm_solana";
+import assert from "assert";
+
+const { SystemProgram } = anchor.web3;
 
 describe("gm-solana", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.GmSolana as Program<GmSolana>;
+  let _baseAccount: anchor.web3.Keypair;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it("creates a base account for gm's", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    const tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    assert.equal(account.gmCount.toString(), "0");
+    _baseAccount = baseAccount;
+  });
+
+  it("receives and saves a gm message",async () => {
+    const message = "gm wagmi";
+    const user = provider.wallet.publicKey;
+    
+    const accountBefore = await program.account.baseAccount.fetch(
+      _baseAccount.publicKey
+    );
+    const gmCountBefore = accountBefore.gmCount;
+
+    const tx = await program.rpc.sayGm(message, {
+      accounts: {
+        baseAccount: _baseAccount.publicKey,
+        user,
+      },
+    });
+
+    const accountAfter = await program.account.baseAccount. fetch(
+      _baseAccount.publicKey
+    );
+
+    const gmCountAfter = accountAfter.gmCount;
+    assert.equal(gmCountAfter.sub(gmCountBefore).toString(), "1");
+
+    const gmList = accountAfter.gmList;
+    assert.equal(gmList[0].message, message);
+    assert.equal(gmList[0].user.equals(user), true);
+    assert.equal(gmList[0].timestamp.gt(new anchor.BN(0)),true);
   });
 });
